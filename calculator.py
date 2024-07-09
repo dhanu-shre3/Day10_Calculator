9d19b1767aa6ff0f55e1addad9079a6d8e49d885
Dhanushree Motanalira Somaiah <motanalirasomaiahd@cardiff.ac.uk>
2024-07-09 19:04:59 +0100

    optimized code

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 #Calculator
-
+from art import logo
 #add
 def add(n1, n2):
     return n1 + n2
@@ -33,28 +33,30 @@ operations = {
         "-":subtract, 
         "*":multiply,  
         "/":divide }
+def calculator():
+    print(logo)
+    num1 = float(input("What is your first number?"))
+    for keys in operations.keys():
+        print (keys)
 
-num1 = int(input("What is your first number?"))
-for keys in operations.keys():
-    print (keys)
-
-to_continue =  True
+    to_continue =  True
 
 
-while to_continue:
-    operator = input("Pick an operation: ")
-    num2 = int(input("What is your next number?"))
-    calculation_function = operations[operator]
-    answer =  calculation_function(num1, num2)
-    
-    print(f"{num1} {operator} {num2} = {answer}")
-    
-    if input(f"Type y to continue with {answer} or n to exit: ") == "y":
-        num1 = answer
-    else:
-        to_continue = False
+    while to_continue:
+        operator = input("Pick an operation: ")
+        num2 = float(input("What is your next number?"))
+        calculation_function = operations[operator]
+        answer =  calculation_function(num1, num2)
 
-             
+        print(f"{num1} {operator} {num2} = {answer}")
+        
+        if input(f"Type y to continue with {answer} or n to exit: ") == "y":
+            num1 = answer
+        else:
+            to_continue = False
+            calculator()
+
+calculator()             
 
     
 
